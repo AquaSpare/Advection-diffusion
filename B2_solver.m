d07d885b9f6684584cbc3aba458e0ae5d81bd78f
AquaSpare <39486044+AquaSpare@users.noreply.github.com>
2020-02-18 20:09:25 +0100

    Big stuff

--- B2_solver.m
+++ B2_solver.m
@@ -35,7 +35,6 @@ while t<T
     
 %     plot(x,u(1,:),'r');
 %     plot(x,u(i,:),'b',x,u(1,:),'r');
-%     pause(k/2)
     
     uend = u(i+1,:);
     
@@ -45,14 +44,18 @@ end
 
 error = norm(uend-u(1,:));
 h = 1./(allN-1);
-error = [1.2450 1.5900 1.2937 0.6844 0.3131];
+error = [1.9283  2.6585 2.5549 2.5204 2.7093];
 
-
+errorN = error./allN;
 %%%%%%%% FUNCTIONS %%%%%%%%
 
 function u0 = init(x) % Initial conditions
 if abs(2*x-0.3) <= 0.25
     u0 = exp(-300*(2*x-0.3)^2);
+elseif abs(2*x-0.9) <= 0.2
+    u0 = 1;
+elseif abs(2*x-1.6) <= 0.2
+    u0 = sqrt(1-((2*x-1.6)/0.2)^2);
 else
     u0 = 0;
 end
@@ -61,7 +64,3 @@ end
 function fdot = runk(A,u,h)
 fdot = -(1/(2*h)).* A*u;
 end
-
-
-
-
